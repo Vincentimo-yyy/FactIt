5bcb0b4391801654c4c49ddf96a59505dd5fb015
Merkiell <radancarl@gmail.com>
2025-04-16 11:07:07 +0800

    await on promise

--- fact-it/app/auth/actions.ts
+++ fact-it/app/auth/actions.ts
@@ -4,7 +4,7 @@ import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
 export async function signIn(formData: FormData) {
-  const supabase = createClient();
+  const supabase = await createClient();
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
 
